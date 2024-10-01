fb45e5804029aac8fdbdaa4a0c6e89780d210bc2
Ankita <yadavankitar@gmail.com>
2024-10-01 02:23:18 -0400

    API Testing

---
+++ AGDataAPIAutomation/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiTestingSuite.Models
+{
+    public class Post
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
